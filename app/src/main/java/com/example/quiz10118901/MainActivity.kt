6a775cb9e0628e0e0b3ae0058cb365704dd99ec1
gonior <arthemuss14@gmail.com>
2021-05-29 12:30:58 +0700

    first commit

---
+++ app/src/main/java/com/example/quiz10118901/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.quiz10118901
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// nim  : 10118901
+// nama : Dedi Cahya
+// Tanggal pengerjaan : Sabtu, 29 Mei 2021
+// Kelas : IF11K
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
